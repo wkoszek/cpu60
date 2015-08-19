4b83a848f59c971765485e1f6b7a680d9345aa3b
Wojciech A. Koszek <wojciech@koszek.com>
2015-08-18 23:55:30 -0700

    Bring the first version of the CPU model.

---
+++ cpu.c
@@ -0,0 +1,60 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+
+#define	MAX(a, b)	((a) > (b)) ? (a) : (b)
+
+int
+main(int argc, char **argv)
+{
+	uint8_t	regs[8];
+	char	buf[1000];
+	int	i, i0, i1, i2, v0, v1, v2, rv, o, reset_perform;
+	char	lines_buf[128][128];
+
+	buf[0] = reset_perform = 0;
+	while ((o = getopt(argc, argv, "r:")) != -1) {
+		switch (o) {
+		case 'r':
+			reset_perform++;
+			break;
+		}
+	}
+
+	do {
+		if (sscanf(buf, "mov r%d,r%d", &i0, &i1) == 2) {
+			regs[i0 % 8] = regs[i1 % 8];
+		}
+		if (sscanf(buf, "add r%d,r%d,r%d", &i0, &i1, &i2) == 3) {
+			regs[i0 % 8] = regs[i1 % 8] + regs[i2 % 8];
+		}
+		if (sscanf(buf, "addi r%d,r%d,%d", &i0, &i1, &v2) == 3) {
+			regs[i0 % 8] = regs[i1 % 8] + v2;
+		}
+		if (sscanf(buf, "sub r%d,r%d,r%d", &i0, &i1, &i2) == 3) {
+			regs[i0 % 8] = regs[i1 % 8] - regs[i2 % 8];
+		}
+		if (sscanf(buf, "subi r%d,r%d,r%d", &i0, &i1, &v2) == 3) {
+			regs[i0 % 8] = regs[i1 % 8] - v2;
+		}
+		if (sscanf(buf, "and r%d,r%d,r%d", &i0, &i1, &i2) == 3) {
+			regs[i0 % 8] = regs[i1 % 8] & regs[i2 % 8];
+		}
+		if (sscanf(buf, "andi r%d,r%d,r%d", &i0, &i1, &v2) == 3) {
+			regs[i0 % 8] = regs[i1 % 8] & v2;
+		}
+		if (sscanf(buf, "or r%d,r%d,r%d", &i0, &i1, &i2) == 3) {
+			regs[i0 % 8] = regs[i1 % 8] | regs[i2 % 8];
+		}
+		if (sscanf(buf, "ori r%d,r%d,r%d", &i0, &i1, &v2) == 3) {
+			regs[i0 % 8] = regs[i1 % 8] | v2;
+		}
+		buf[MAX(0, strlen(buf) - 1)] = 0;
+		for (i = 0; i < 8; i++) {
+			printf("reg%1d=%02x %s %s", i, regs[i],
+			    i == 7 ? buf : "",
+			    i == 7 ? "\n":" ");
+		}
+	} while (fgets(buf, sizeof(buf), stdin));
+}
